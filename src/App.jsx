5bdbe1a3f969cbd40e90f5be34951d387005000a
NMGVox <nijeemgreene@gmail.com>
2023-08-31 15:11:16 -0400

    Add a timing object for later use

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react'
+// import Timer from './components/timer'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className="">
+    </div>
   )
 }
 
---
+++ src/components/timer.jsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react'
+
+function Timer() {
+  const [timer, setTimer] = useState(0);
+
+  useEffect(() => {
+    const gameTimer = setInterval(() => {
+      setTimer(timer + 1);
+    }, 1000)
+
+    return (()=> {
+      clearInterval(gameTimer);
+    })
+  }, [timer]);
+
+  return (
+    <h1>{`${Math.floor(timer / 60)} : ${String(timer % 60).padStart(2, '0')}`}</h1>
+  )
+}
+
+export default Timer

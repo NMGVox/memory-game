150b028cd449229a657692d0f39bdad399a566ae
NMGVox <nijeemgreene@gmail.com>
2023-09-01 15:52:06 -0400

    Fetch data from Deezer API and display
    Using a list of search queries, the app can now request and display data
    retrieved from the Deezer API.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,48 @@
 import { useState, useEffect } from 'react'
+import { dataList as albums }  from './data/data'
 // import Timer from './components/timer'
 import './App.css'
 
 function App() {
+  const [albumInfo, setAlbumInfo] = useState([]);
+
+  function fetchAlbumInfo() {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'c78f458c76msh16d4ce41cccd8ddp17bd48jsn04deb672eb9d',
+        'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+      }
+    };
+    const covers = []
+    albums.map(async (album) => {
+      try{
+        const url = `https://deezerdevs-deezer.p.rapidapi.com/search?q=${album}`;
+        const data = await fetch(url, options);
+        const result = await data.json();
+        covers.push(result.data[0].album);
+        setAlbumInfo(covers);
+      }
+      catch (error) {
+        console.log(error.message);
+      }
+    });
+  }
+
+
+  useEffect(() => {
+    fetchAlbumInfo()
+  }, [])
+
 
   return (
-    <div className="">
+    <div className="imageGrid">
+      {albumInfo.map((album) => (
+        <div key={album.id} className="albumWrapper">
+          <img src={album.cover_medium} alt="" />
+          <h1>{album.title}</h1>
+        </div>
+      ))}
     </div>
   )
 }
---
+++ src/data/data.js
@@ -0,0 +1,24 @@
+const dataList = [
+    'Stone Temple Pilots Core',
+    'Stone Temple Pilots Tiny Music',
+    'Michael Jackson Bad',
+    'Judas Priset Painkiller',
+    'Faith No More King For A Day',
+    'Queens of the Stone Age Paralyze',
+    'Queens of the Stone Age Deaf',
+    'Hall and Oates Voices',
+    'Nirvana Bleach',
+    'Disturbed Indestructible',
+    'SlipKnot Psychosocial',
+    'Alice in Chains Jar of Flies',
+    'Soundgarden Badmotofinger',
+    'Lady Gaga Fame Monster',
+    'El-Creepo Aloha',
+    'Gorillaz Plastic Beach',
+    'Devo Are we not men',
+    'System of a Down Steal This Album',
+    'Oingo Boingo Dead Mans Party',
+    'Kaiser Chiefs Yours Truly'
+]
+
+export {dataList}

92fdb8258cfdf75d693a7db0d1111cfddebcc1cc
NMGVox <nijeemgreene@gmail.com>
2023-09-08 15:26:39 -0400

    Revamp the win/loss screen

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -4,7 +4,7 @@ export default function Results( { mainMenu, startGame, advanceLevel, diff, resu
             <h1 className="resultText">You {result === 'win' ? `won!` : `lost!`}</h1>
             <span className="buttonContainer verticalFlex">
                 <button onClick={startGame} className="resultsButton">Restart</button>
-                {(diff !== 'hard') && <button onClick={advanceLevel} className="resultsButton">Next Difficulty</button>}
+                {(diff !== 'hard' && result === 'win') && <button onClick={advanceLevel} className="resultsButton">Next Difficulty</button>}
                 <button onClick={mainMenu} className="resultsButton">Main Menu</button>
             </span>
         </div>
